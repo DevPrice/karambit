f6452121d1a7613ac1baab0abee48c8366993bdd
Devin Price <devin@nowyoujump.com>
2024-09-29 22:46:40 -0700

    fix sample project

--- samples/hello_world/src/index.ts
+++ samples/hello_world/src/index.ts
@@ -1,29 +1,4 @@
-import {Inject, Module, Provides, Component, createComponent} from "karambit-inject"
+import {KarambitHelloWorldComponent} from "./karambit-generated/src/component"
 
-@Inject
-class Greeter {
-
-    constructor(private readonly greeting: string) { }
-
-    greet(): string {
-        return `${this.greeting}, World!`
-    }
-}
-
-@Module
-abstract class HelloWorldModule {
-
-    @Provides
-    static provideGreeting(): string {
-        return "Hello"
-    }
-}
-
-@Component({modules: [HelloWorldModule]})
-abstract class HelloWorldComponent {
-
-    abstract readonly greeter: Greeter
-}
-
-const component = createComponent<typeof HelloWorldComponent>()
+const component = new KarambitHelloWorldComponent()
 console.log(component.greeter.greet())
