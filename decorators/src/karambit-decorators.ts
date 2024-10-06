fe40bfddff2e01ef3ff7d8c12378134cd4328c4e
Devin Price <devin@nowyoujump.com>
2024-10-05 21:20:25 -0700

    brand all karambit annotations

--- decorators/src/karambit-decorators.ts
+++ decorators/src/karambit-decorators.ts
@@ -81,29 +81,30 @@ export interface MultibindingOptions {
     optional: boolean
 }
 
-export const Component: ClassAnnotation<[] | [ComponentInfo]> = ClassAnnotation
-export const Subcomponent: ClassAnnotation<[] | [SubcomponentInfo]> = ClassAnnotation
-export const Module: ClassAnnotation<[] | [ModuleInfo]> = ClassAnnotation
-export const Inject: ClassAnnotation = ClassAnnotation
-export const AssistedInject: ClassAnnotation = ClassAnnotation
-export const Assisted: ParameterAnnotation = ParameterAnnotation
-export const Provides: MemberAnnotation = MemberAnnotation
-export const Binds: MemberAnnotation = MemberAnnotation
-export const BindsInstance: ParameterAnnotation = ParameterAnnotation
-export const IntoSet: MemberAnnotation<[] | [MultibindingOptions]> = MemberAnnotation
-export const IntoMap: MemberAnnotation<[] | [MultibindingOptions]> = MemberAnnotation
-export const ElementsIntoSet: MemberAnnotation = MemberAnnotation
-export const ElementsIntoMap: MemberAnnotation = MemberAnnotation
-export const MapKey: GenericMemberAnnotation = MemberAnnotation
+type KarambitAnnotationBrand<T extends string> = {
+    readonly [key in `__karambit${T}Annotation`]?: unknown;
+}
 
-export interface ScopeAnnotation extends ClassAnnotation, MemberAnnotation {
+export const Component: ClassAnnotation<[] | [ComponentInfo]> & KarambitAnnotationBrand<"Component"> = ClassAnnotation
+export const Subcomponent: ClassAnnotation<[] | [SubcomponentInfo]> & KarambitAnnotationBrand<"Subcomponent"> = ClassAnnotation
+export const Module: ClassAnnotation<[] | [ModuleInfo]> & KarambitAnnotationBrand<"Module"> = ClassAnnotation
+export const Inject: ClassAnnotation & KarambitAnnotationBrand<"Inject"> = ClassAnnotation
+export const AssistedInject: ClassAnnotation & KarambitAnnotationBrand<"AssistedInject"> = ClassAnnotation
+export const Assisted: ParameterAnnotation & KarambitAnnotationBrand<"Assisted"> = ParameterAnnotation
+export const Provides: MemberAnnotation & KarambitAnnotationBrand<"Provides"> = MemberAnnotation
+export const Binds: MemberAnnotation & KarambitAnnotationBrand<"Binds"> = MemberAnnotation
+export const BindsInstance: ParameterAnnotation & KarambitAnnotationBrand<"BindsInstance"> = ParameterAnnotation
+export const IntoSet: MemberAnnotation<[] | [MultibindingOptions]> & KarambitAnnotationBrand<"IntoSet"> = MemberAnnotation
+export const IntoMap: MemberAnnotation<[] | [MultibindingOptions]> & KarambitAnnotationBrand<"IntoMap"> = MemberAnnotation
+export const ElementsIntoSet: MemberAnnotation & KarambitAnnotationBrand<"ElementsIntoSet"> = MemberAnnotation
+export const ElementsIntoMap: MemberAnnotation & KarambitAnnotationBrand<"ElementsIntoMap"> = MemberAnnotation
+export const MapKey: GenericMemberAnnotation & KarambitAnnotationBrand<"MapKey"> = MemberAnnotation
+
+export interface ScopeAnnotation extends ClassAnnotation, MemberAnnotation, KarambitAnnotationBrand<"Scope"> {
     (): ClassDecorator & MemberDecorator
-    __karambitScopeAnnotation?: unknown
 }
 
-export interface ReusableScopeAnnotation extends ScopeAnnotation {
-    __karambitReusableScopeAnnotation?: unknown
-}
+export type ReusableScopeAnnotation = ScopeAnnotation & KarambitAnnotationBrand<"ReusableScope">
 
 function ScopeAnnotation(target?: any): ScopeAnnotation {
     if (typeof target === "function") return target
