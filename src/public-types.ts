1db64e8ea96e3565fbefad91440a8b29f34e2ad7
Devin <devin@nowyoujump.com>
2025-12-06 17:47:00 -0800

    export error types

--- src/public-types.ts
+++ src/public-types.ts
@@ -1,4 +1,5 @@
 import * as ts from "typescript"
+export {KarambitError, KarambitErrorScope} from "./KarambitError"
 
 export type Logger = Pick<typeof console, "debug" | "info" | "warn" | "error">
 export type ComponentDeclaration = ts.ClassDeclaration | ts.InterfaceDeclaration
